a586bff091cee3c76c0c7e7d3df4c457ab231606
krishnayadavengineer-ai <krishnaydav.engineer@gmail.com>
2026-04-28 14:29:18 +0530

    added new Package and named as Java17Features in that added 1 class on these time added more features of Java17 and So......

---
+++ src/Java17Features/SealedClassDemo1.java
@@ -0,0 +1,36 @@
+package Java17Features;
+
+sealed class Vechile permits Car{
+    void method1(){
+        System.out.println("Hello method1");
+    }
+
+}
+
+non-sealed class Car extends Vechile{
+    void method1(){
+        System.out.println("From Car Hello method1");
+    }
+
+}
+final class Bhike extends Car{
+    void method1(){
+        System.out.println("From Bhike Hello method1");
+    }
+
+}
+
+
+
+
+public class SealedClassDemo1 {
+    public static void main(String[] args) {
+
+        Vechile v1 = new Car();
+        v1.method1();
+
+        Vechile v2 = new Bhike();
+        v2.method1();
+
+    }
+}
